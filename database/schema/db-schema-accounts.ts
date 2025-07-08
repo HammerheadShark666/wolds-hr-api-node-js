86cb72163a53335b72b4ebc8d6288e099bcecf8e
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2025-07-08 18:36:31 +0100

    first commit

---
+++ database/schema/db-schema-accounts.ts
@@ -0,0 +1,19 @@
+export const accountSchema = {
+  title: 'task schema',
+  version: 0,
+  description: 'describes a task',
+  type: 'object',
+  primaryKey: 'id',
+  properties: {
+    id: {
+      type: 'string',
+      maxLength: 100
+    },
+    task: {
+      type: 'string',
+      maxLength: 255
+    },
+    completed: { type: 'boolean' },
+  },
+  required: ['id', 'task'],
+};
