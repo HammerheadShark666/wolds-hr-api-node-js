f03d0ae8a7d7820eb31d138b0afcb0dab88eaa11
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2025-07-12 17:26:38 +0100

    Add department routes

--- src/routes/department.ts
+++ src/routes/department.ts
@@ -11,9 +11,16 @@ export function createDepartmentRouter(db: RxDatabase<WoldsHrDatabaseCollections
 
   router.post('/', async (req, res) => {
     try {
+    
+      const name = req.body.name;    
+      const doc = await db.departments.findOne({ selector: { name } }).exec();
+
+      if (doc) {
+        return res.status(400).json({ error: 'Department already exists' });
+      }
+
       const id = uuidv4();
-      const departmentName = req.body.name;   
-      const newDepartment: ApiDepartment = { id: id, name: departmentName , _meta: { lwt: Date.now() }, _deleted: false, _attachments: {}, _rev: "test-data" };
+      const newDepartment: ApiDepartment = { id: id, name: name , _meta: { lwt: Date.now() }, _deleted: false, _attachments: {}, _rev: "test-data" };
       const response = await db.departments.insert(newDepartment);     
       res.status(200).json(response);
     } catch (err: unknown) {
@@ -45,6 +52,26 @@ export function createDepartmentRouter(db: RxDatabase<WoldsHrDatabaseCollections
       res.status(404).json({ message: 'Department not found' });
     }
   });
+
+  router.delete('/:id', async (req, res) => {
+
+    try {
+
+      const id = req.params.id.toString();
+      const doc = await db.departments.findOne({ selector: { id } }).exec();
+
+      if (!doc) {
+        return res.status(404).json({ error: 'Department not found' });
+      }
+
+      await doc.remove();
+
+      res.status(200).json({ message: 'Department deleted' });
+    } catch (error) {
+      console.error('Delete failed:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    } 
+  }); 
 
   return router;
 }
