476755277f6939056ead62a083c39376add311f7
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2025-07-14 18:19:36 +0100

    Clean up

--- src/app.ts
+++ src/app.ts
@@ -11,60 +11,32 @@ import listEndpoints from 'express-list-endpoints';
 
 export async function createApp() {
 
-   const app = express();
+  const app = express();
 
   try 
-  {
- 
-  app.use(cors()); 
-  app.use(cookieParser());  
-  app.use(express.json()); 
- 
-  const db = await createDb();
-
-  const v1Router = express.Router();
-
-  v1Router.use('', createAuthenticationRouter(db));
-  v1Router.use((req, res, next) => {
-  console.log(`[v1Router] ${req.method} ${req.path}`);
-  next();
-});
-  v1Router.use(authenticateToken);
-  v1Router.use((req, res, next) => {
-  console.log(`[v1Router] ${req.method} ${req.path}`);
-  next();
-});
-  v1Router.use('/departments', createDepartmentRouter(db));
-  v1Router.use((req, res, next) => {
-  console.log(`[v1Router] ${req.method} ${req.path}`);
-  next();
-});
-  v1Router.use('/employees', createEmployeeRouter(db));  
-  v1Router.use((req, res, next) => {
-  console.log(`[v1Router] ${req.method} ${req.path}`);
-  next();
-});
-  v1Router.use('/accounts', createAccountsRouter(db));
-
-app.use('/v1', (req, res, next) => {
-  console.log(`[app] Incoming request ${req.method} ${req.originalUrl}`);
-  next();
-});
-
-  app.use('/v1', v1Router);
-
-  // app.use('', v1Router);
-
+  { 
+    app.use(cors()); 
+    app.use(cookieParser());  
+    app.use(express.json()); 
   
-console.log('--- Listing all endpoints: ---');
-console.log(listEndpoints(app));
-console.log('--- End of endpoint list ---');
+    const db = await createDb();
+    const v1Router = express.Router();
+
+    v1Router.use('', createAuthenticationRouter(db)); 
+    v1Router.use(authenticateToken); 
+    v1Router.use('/departments', createDepartmentRouter(db)); 
+    v1Router.use('/employees', createEmployeeRouter(db));  
+    v1Router.use('/accounts', createAccountsRouter(db));
+  
+    app.use('/v1', v1Router);  
+
+    console.log('--- Listing all endpoints: ---');
+    console.log(listEndpoints(app));
+    console.log('--- End of endpoint list ---'); 
+
+  } catch (err) {
+    console.error('❌ CreateApp error:', err); 
+  }
 
   return app;
-
-    } catch (err) {
-      console.error('❌ CreateApp error:', err); 
-    }
-
-    return app;
 } 
