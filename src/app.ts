330ba7a107de322e330710550fe3398a7ed4421d
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2025-07-14 14:06:56 +0100

    Add logging to find details about path issue

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import { createAccountsRouter } from './routes/account';
 import cors from 'cors';
 import { authenticateToken } from './middleware/authenticateToken';
 import cookieParser from 'cookie-parser';
+import listEndpoints from 'express-list-endpoints';
 
 export async function createApp() {
   const app = express();
@@ -19,12 +20,31 @@ export async function createApp() {
   const v1Router = express.Router();
 
   v1Router.use('', createAuthenticationRouter(db));
+  v1Router.use((req, res, next) => {
+  console.log(`[v1Router] ${req.method} ${req.path}`);
+  next();
+});
   v1Router.use(authenticateToken);
+  v1Router.use((req, res, next) => {
+  console.log(`[v1Router] ${req.method} ${req.path}`);
+  next();
+});
   v1Router.use('/departments', createDepartmentRouter(db));
+  v1Router.use((req, res, next) => {
+  console.log(`[v1Router] ${req.method} ${req.path}`);
+  next();
+});
   v1Router.use('/employees', createEmployeeRouter(db));  
+  v1Router.use((req, res, next) => {
+  console.log(`[v1Router] ${req.method} ${req.path}`);
+  next();
+});
   v1Router.use('/accounts', createAccountsRouter(db));
 
   app.use('/v1', v1Router);
+
+  
+console.log(listEndpoints(app));
 
   return app;
 } 
