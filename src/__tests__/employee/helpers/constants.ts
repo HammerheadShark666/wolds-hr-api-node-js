7b1413a0a338127e00dc3ce8126716929e33333f
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2025-08-12 13:45:18 +0100

    Move uuid regex constant

--- src/__tests__/employee/helpers/constants.ts
+++ src/__tests__/employee/helpers/constants.ts
@@ -5,3 +5,5 @@ export const EMPLOYEE_HIRE_DATE = new Date("03-11-2021");
 export const EMPLOYEE_EMAIL = "test@hotmail.com";
 export const EMPLOYEE_PHONE_NUMBER = "0177563423"; 
 export const EMPLOYEE_DEPARTMENT_ID = "687783fbb6fc23ad4cdca63e";
+
+export const UUID_REGEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
